3685de73edb3a47de5dedc23eaaa74017948954e
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:12:56 +0000

    Automatically backed up by Learn

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,4 +1,19 @@
 # Implement your object-oriented solution here!
 class SumSquareDifference 
   
+  def initialize(number) 
+    @number = number 
+  end 
+  
+  def sum_square_difference 
+    squares = []
+    sum_of_squares = 0
+    i = 1
+    @number.times do 
+      squares << i ** 2 
+      sum_of_squares += i
+      i += 1 
+    end
+    sum_of_squares ** 2 - squares.sum
+  end
 end 
