b2be587084e835f4b5163d82f590eb2edd71770c
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 15:13:16 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference 
+  
+  def initialize(number) 
+    @number = number 
+  end 
+  
+  def difference 
+    squares = []
+    sum_of_squares = 0
+    i = 1
+    @number.times do 
+      squares << i ** 2 
+      sum_of_squares += i
+      i += 1 
+    end
+    sum_of_squares ** 2 - squares.sum
+  end
+end 
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,12 @@
 # Implement your procedural solution here!
+def sum_square_difference(number) 
+  squares = []
+  sum_of_squares = 0
+  i = 1
+  number.times do 
+    squares << i ** 2 
+    sum_of_squares += i
+    i += 1 
+  end
+  sum_of_squares ** 2 - squares.sum
+end
